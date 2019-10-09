33ab516067347f1c79e694fcb82558306a7b48ea
Adrian Salgado <asalgado@santander.com.mx>
2019-10-09 16:31:06 -0500

    Notifications paginator

--- src/main/java/mx/com/sadead/store/notification/service/NotificationService.java
+++ src/main/java/mx/com/sadead/store/notification/service/NotificationService.java
@@ -5,10 +5,11 @@ import mx.com.sadead.store.notification.repository.NotificationRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -40,12 +41,13 @@ public class NotificationService {
     /**
      * Get all the notifications.
      *
+     * @param pageable the pagination information.
      * @return the list of entities.
      */
     @Transactional(readOnly = true)
-    public List<Notification> findAll() {
+    public Page<Notification> findAll(Pageable pageable) {
         log.debug("Request to get all Notifications");
-        return notificationRepository.findAll();
+        return notificationRepository.findAll(pageable);
     }
 
 
